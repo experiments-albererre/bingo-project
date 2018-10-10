1d4391dcfcd650daba63047482a034dc99f30331
Alber Erre <a.ruizblugo@gmail.com>
2018-10-10 21:02:03 +0100

    sweet alert and winner logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-// Lowdash library
+// libraries
 const _ = require('lodash');
 const vanillaTilt = require('vanilla-tilt');
+const swal = require('sweetalert2');
 
 VanillaTilt.init(document.querySelector(".playerContainer"), {
     max: 25,
@@ -24,25 +25,65 @@ const machine = document.querySelector(".cpuContainer");
 let started = false;
 let humanNumbers = [];
 let machineNumbers = [];
+let gameNumbers = [];
+let humanCounter = 0;
+let machineCounter = 0;
 
-let randomNumber = () => {
+//Functions
+
+let finishGame = (_msg) => {
+
+    let bol = swal({
+        title: _msg,
+        text: 'Do you want to play again?',
+        type: 'error',
+        confirmButtonText: 'Yeah'
+      }).then((result) => {
+        if (result.value) {
+            // refresh page!
+            location.reload();
+        }
+      })
+
+}
+
+let checkWin = () => {
+
+    (humanCounter >= 15 && machineCounter >= 15 ? finishGame("Tables!") : true);
+    (humanCounter >= 15 ? finishGame("Human has won") : true);
+    (machineCounter >= 15 ? finishGame("Machine has won") : true);
+}
+
+let randomNumber = (_event) => {
   
-  //check if this is the first time you push button
-  (started == false ? startGame() : true );
+    // check if this is the first time you push button
+    (started == false ? startGame() : true );
 
-  let number = Math.round( (Math.random() * 89 ) + 1);
-  dice.innerHTML = number;
+    // use array to avoid repeat numbers played
+    let number = gameNumbers[gameNumbers.length - 1];
+    gameNumbers.pop();
 
-  // cross the number in cards
-  crossNumber(number);
+    dice.innerHTML = number;
+    crossNumber(number);
+    checkWin();
 }
 
 let crossNumber = (_number) => {
 
-    console.log(_number);
+    if ( document.querySelector(`.number.n${_number}`) ) {
+        let num = document.querySelectorAll(`.number.n${_number}`);
 
-    let num = document.querySelector(`.number.n${_number}`);
-    num.classList.add("tachado");
+        for (let i = 0; i < num.length; i++) {
+            num[i].classList.add("tachado");
+        }
+
+        // add counters based on CSS condition
+        (document.querySelector(`.playerContainer .number.n${_number}`) ? humanCounter = humanCounter + 1 : true);
+        console.log(humanCounter);
+    
+        (document.querySelector(`.cpuContainer .number.n${_number}`) ? machineCounter = machineCounter + 1 : true);
+        console.log(machineCounter);
+    }
 }
 
 let startGame = () => {
@@ -56,7 +97,7 @@ let startGame = () => {
 let showNumbers = (_array, _domElement) => {
 
     _array.forEach((e) => {
-        
+        // create and add element in DOM
         let div = document.createElement("div");
         div.className = `number n${e}`;
         div.textContent = e;
@@ -73,10 +114,13 @@ let generateCards = (_array) => {
         // push numbers in _array, avoiding duplicates (every one is unique)
         _array.push(shuffledRange[i]);
     }
+
+    //mix numbers again and assign them as game numbers
+    gameNumbers = _.shuffle(shuffledRange);
 }
 
-button.onclick = () => randomNumber();
+//Interactivity
 
-
-
+button.addEventListener("click", (event) => randomNumber(event) );
+//button.onclick = () => randomNumber();
 

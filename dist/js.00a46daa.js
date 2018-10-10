877cb9ebfa0f2658dd56e8c2bfec8d2c3ad74c16
Alber Erre <a.ruizblugo@gmail.com>
2018-10-10 18:08:52 +0100

    styling added

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -19677,7 +19677,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38915" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40269" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
